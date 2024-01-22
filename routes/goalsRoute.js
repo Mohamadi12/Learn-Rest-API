5ed7518cfae9253980b6925aa403993d38c7c251
Mohamadi12 <adilnana78@gmail.com>
2024-01-22 16:46:53 +0100

    lera

---
+++ routes/goalsRoute.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const router=express.Router()
+const {getGoals,postGoal,putGoal,deleteGoal}=require('../controllers/goalController')
+
+router.get('/',getGoals)
+router.post('/',postGoal)
+router.put('/:id',putGoal)
+router.delete('/:id',deleteGoal)
+
+module.exports=router
---
+++ service.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express=require('express')
+const cors=require('cors')
+const router = require('./routes/goalsRoute')
+const  errorHandler =require('./middleware/errorMiddleware')
+const connectDB = require('./config/connectDB')
+const Port=5000 || process.env.port
+
+const app=express()
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use('/api/goals',router)
+app.use(errorHandler)
+connectDB()
+
+app.listen(Port,(err)=>{
+    err? console.log(err):console.log(`Yes successfull ${Port}`)
+})
